053739cf2b2a563343d6e00c52933c6738bce346
Alsu Abdrakhmanova <dxdy10@mail.ru>
2020-11-21 13:20:17 +0500

    fix: div className=place-card is added in Card

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,13 +1,15 @@
 function Card(props) {
+    const isLiked = false;
+    const btnClassName = `place-card__like-icon ${isLiked ? 'place-card__like-icon_liked' : ''}`;
     return (
-        <div class="place-card">
+        <div className="place-card">
             <div className="place-card__image" style={{ backgroundImage: `url(${props.card.link})` }}>
                 <button className="place-card__delete-icon"></button>
             </div>
             <div className="place-card__description">
                 <h3 className="place-card__name">{props.card.name}</h3>
                 <div className="place-card__right-container">
-                    <button className="place-card__like-icon"></button>
+                    <button className={btnClassName} />
                     <p className="place-card__count">{props.card.likes.length}</p>
                 </div>
             </div>
