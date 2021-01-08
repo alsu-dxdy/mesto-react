8cc8bd4befd59ffb1abbfd225c4e089d0e7c45f2
Alsu Abdrakhmanova <dxdy10@mail.ru>
2021-01-08 17:43:56 +0500

    add: CurrentUserContext, cardDeleteButtonClassName, cardLikeButtonClassName in Card

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,7 +1,21 @@
+import React from 'react';
+import { CurrentUserContext } from '../contexts/CurrentUserContext';
+
 function Card(props) {
     const { card, onCardClick } = props;
-    const isLiked = false;
-    const btnClassName = `place-card__like-icon ${isLiked ? 'place-card__like-icon_liked' : ''}`;
+    const currentUser = React.useContext(CurrentUserContext);
+    // Определяем, являемся ли мы владельцем текущей карточки
+    const isOwn = card.owner._id === currentUser._id;
+
+    // Создаём переменную, которую после зададим в `className` для кнопки удаления
+    const cardDeleteButtonClassName = (
+        `place-card__delete-icon ${isOwn ? 'place-card__delete-icon_visible' : ''}`
+    );
+
+    // Определяем, есть ли у карточки лайк, поставленный текущим пользователем
+    const isLiked = card.likes.some(i => i._id === currentUser._id);
+    // Создаём переменную, которую после зададим в `className` для кнопки лайка
+    const cardLikeButtonClassName = `place-card__like-icon ${isLiked ? 'place-card__like-icon_liked' : ''}`;
 
     function handleClick() {
         onCardClick(card);
@@ -10,12 +24,12 @@ function Card(props) {
     return (
         <div className="place-card">
             <div className="place-card__image" onClick={handleClick} style={{ backgroundImage: `url(${card.link})` }}>
-                <button className="place-card__delete-icon"></button>
+                <button className={cardDeleteButtonClassName}></button>
             </div>
             <div className="place-card__description">
                 <h3 className="place-card__name">{card.name}</h3>
                 <div className="place-card__right-container">
-                    <button className={btnClassName} />
+                    <button className={cardLikeButtonClassName} />
                     <p className="place-card__count">{card.likes.length}</p>
                 </div>
             </div>
